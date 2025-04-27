194a9f6881527d6a1b372d780d1a7b8e378aa125
ranjeeth acharya <101057964+RanjeethAcharya@users.noreply.github.com>
2025-04-27 14:50:31 +0530

    Update install.sh

--- terraform/install.sh
+++ terraform/install.sh
@@ -23,6 +23,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker jenkins
 sudo chown root:docker /var/run/docker.sock
 sudo chmod 660 /var/run/docker.sock
